2832b3e99233f1abe342adacdb84f3367532dfe2
Mateus Luiz <mateuslfreitasb@gmail.com>
2019-01-07 18:59:02 -0200

    Refactor bob

--- bob/bob.exs
+++ bob/bob.exs
@@ -1,30 +1,37 @@
 defmodule Bob do
 
-  def uppercase_question?(text) do
-    String.match? text, ~r/([A-Z]\?)/
+  def uppercase?(text), do: text == String.upcase(text)
+
+  def question?(text), do: String.ends_with?(text, "?")
+
+  def shout?(text), do: String.ends_with?(text, "!")
+
+  def blank?(text), do: String.trim(text) == ""
+
+  def only_letters?(text), do: String.match?(text, ~r/\p{L}/)
+
+  def simple_question?(text) do
+    question?(text)
   end
 
-  def question?(text) do
-    String.ends_with? text, "?"
+  def uppercase_question?(text) do
+    question?(text) && uppercase?(text) && only_letters?(text)
   end
 
   def uppercase_shouting?(text) do
-    String.match? text, ~r/(\b\p{Lu}+\b)/
+    uppercase?(text) && only_letters?(text)
   end
 
-  def blank_statment?(text) do
-    String.trim(text) == ""
-  end
 
   def hey(input) do
     cond do
       uppercase_question?(input) ->
         "Calm down, I know what I'm doing!"
+      simple_question?(input) ->
+        "Sure."
       uppercase_shouting?(input) ->
         "Whoa, chill out!"
-      question?(input) ->
-        "Sure."
-      blank_statment?(input) ->
+      blank?(input) ->
         "Fine. Be that way!"
       true ->
         "Whatever."
