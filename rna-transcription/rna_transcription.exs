20f4d7bd215efe68608240d560a0ef896bcdde7d
Mateus Luiz <mateuslfreitasb@gmail.com>
2019-01-21 15:59:37 -0200

    Implement the solution

--- rna-transcription/rna_transcription.exs
+++ rna-transcription/rna_transcription.exs
@@ -1,4 +1,20 @@
 defmodule RNATranscription do
+
+  defp transcribe_nucleotide(71), do: 'C'
+
+  defp transcribe_nucleotide(67), do: 'G'
+
+  defp transcribe_nucleotide(84), do: 'A'
+
+  defp transcribe_nucleotide(65), do: 'U'
+
+  defp transcribe_dna_sequence([]), do: []
+
+  defp transcribe_dna_sequence(dna) do
+    [head | tail] = dna
+    transcribe_nucleotide(head) ++ transcribe_dna_sequence(tail)
+  end
+
   @doc """
   Transcribes a character list representing DNA nucleotides to RNA
 
@@ -9,5 +25,6 @@ defmodule RNATranscription do
   """
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
+    to_charlist transcribe_dna_sequence(dna)
   end
 end
